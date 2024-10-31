fe41d5c2ef3e86084fbd9e9e57156df90cc0c593
Hanics Mihály Péter <hanicsm@gmail.com>
2024-10-31 18:48:30 +0100

    add file

---
+++ analytics_table.sql
@@ -0,0 +1,4 @@
+-- We already filled up the separate tables with "mined" data (e.g. finding years from the style string, comma separations),
+-- so most of the preparation is done
+-- Here we create a table for analytics, with various dimensions to support analysis
+
